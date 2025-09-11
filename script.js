ea29100cbbec89c9e7f97e014e228fe9833b0b98
sainihusan <Sainihusan02@gmail.com>
2025-09-11 17:24:00 +0530

    set code in professional way

--- script.js
+++ script.js
@@ -2,9 +2,43 @@ const input = document.getElementById('todo-input');
 const ul= document.getElementById('todoList');
 const button = document.getElementById('add-btn');
 const form = document.getElementById('todo-form');
-const countSpan = document.getElementById('task-count');
+const countdisplay = document.getElementById('task-count');
 const clearAllBtn = document.getElementById('clear-all-btn');
+const errorDisplay = document.getElementById('error-display')
 
+let todoItems = [];
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  addTask()
+  taskCounter()
+})
+
+// function to add new task 
+function addTask(){
+  if(input.value.length !== 0){
+
+    const uuid = guidGenerator()
+    const inputValue = input.value.trim();
+    const newTodo = {id: uuid, task: inputValue}
+    todoItems.push(newTodo);
+
+    const li = document.createElement('li');
+    li.setAttribute('id', newTodo.id);
+    li.textContent = inputValue;
+    li.className = "list"
+    ul.appendChild(li);
+    renderButtons(li);
+    input.value=""
+    addTodos(todoItems)
+  }
+  else{
+
+  }
+}
+
+// task ids
 function guidGenerator() {
     var S4 = function() {
        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
@@ -12,8 +46,36 @@ function guidGenerator() {
     return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
 
-let todoListValue = [];
 
+  // saving to localstorage
+  const getTodos = () => {
+    return JSON.parse(localStorage.getItem("todos"))|| [];
+}
+  const addTodos = (task) =>{
+     return localStorage.setItem('todos', JSON.stringify(task))
+}
+
+
+// showtodolist
+const showTodoList =() => {
+  todoItems=getTodos();
+  todoItems.forEach((curTodo) => {
+    
+    const li = document.createElement('li');
+  li.textContent = curTodo.task;
+  
+  li.className = "list"
+  li.setAttribute('id', curTodo.id)
+  ul.appendChild(li);
+  renderButtons(li);
+  input.value=""
+
+  taskCounter();
+  });
+};
+
+
+// renderbuttons
 
 const renderButtons = (li) => {
     const div = document.createElement('div')
@@ -27,138 +89,84 @@ const renderButtons = (li) => {
     div.appendChild(deleteButton)
     div.appendChild(editButton)
     li.appendChild(div)
-   
 
+   
+// deletebutton
     deleteButton.addEventListener('click',() => {
     li.remove();
-    updateCount();
-    
-    
+    taskCounter();    
   });
-editButton.addEventListener('click', () => {
-  let liValue;
-    for(let i=0; i<todoListValue.length; i++){
-        if(todoListValue[i].id === li.id){
-          liValue = todoListValue[i].task
+  editButton.addEventListener('click', () => handleEdit(li, editButton)); 
+};
+
+// handling edit
+const handleEdit = (li, editButton) => {
+    let taskText;
+    for (let i = 0; i < todoItems.length; i++) {
+        if (todoItems[i].id === li.id) {
+            taskText = todoItems[i].task;
         }
-      }
-
-    if (editButton.textContent == "Edit") {
-      console.log(liValue, li.id)
-      const addText = li.firstChild.textContent;
-      const input = document.createElement('input');
-      input.className = "edit-input"
-      input.type = "text";
-      input.value = liValue;
-
-
-
-      li.firstChild.replaceWith(input);
-      editButton.textContent = "Save";
-    } else {
-      
-      const input = li.firstChild;
-      const newText = document.createTextNode(input.value);
-
-      input.replaceWith(newText);
-      let newArray = []
-      for(let i=0; i<todoListValue.length; i++){
-        if(todoListValue[i].id === li.id){
-          newArray.push({id:todoListValue[i].id ,task: input.value})
-        } else{
-
-          newArray.push(todoListValue[i])
-        }
-      }
-
-      addToDOLocalStorage(newArray)
-      
-      editButton.textContent = "Edit";
-     
-     
     }
-  });
+
+    if (editButton.textContent === "Edit") {
+        
+        const input = document.createElement('input');
+        input.className = "edit-input";
+        input.type = "text";
+        input.value = taskText;
+        li.firstChild.replaceWith(input);
+        editButton.textContent = "Save";
+    } else {
+        
+        const input = li.firstChild;
+        const newText = document.createTextNode(input.value);
+        input.replaceWith(newText);
+
+        
+        let newArray = [];
+        for (let i = 0; i < todoItems.length; i++) {
+            if (todoItems[i].id === li.id) {
+                newArray.push({ id: todoItems[i].id, task: input.value });
+            } else {
+                newArray.push(todoItems[i]);
+            }
+        }
+        addTodos(newArray);
+        editButton.textContent = "Edit";
+    }
 };
 
-
-
-form.addEventListener('submit', function(e) {
-  e.preventDefault()
-  addTask()
-  updateCount()
-})
-
-
-const getToDoLocalStorage = () => {
-  return JSON.parse(localStorage.getItem("todos"))|| [];
-}
-const addToDOLocalStorage = (task) =>{
-  return localStorage.setItem('todos', JSON.stringify(task))
-}
-
-
-const showTodoList =() => {
-  todoListValue=getToDoLocalStorage();
-  todoListValue.forEach((curTodo) => {
-    
-    const li = document.createElement('li');
-  li.textContent = curTodo.task;
   
-  li.className = "list"
-  li.setAttribute('id', curTodo.id)
-  ul.appendChild(li);
-  renderButtons(li);
-  input.value=""
-
-  updateCount();
-  
-    
-  });
-
-};
-
-function addTask(){
-  if(input.value.length !== 0){
-
-    const uuid = guidGenerator()
-    const inputValue = input.value.trim();
-    const newTodo = {id: uuid, task: inputValue}
-    todoListValue.push(newTodo);
-
-    const li = document.createElement('li');
-    li.setAttribute('id', newTodo.id);
-    li.textContent = inputValue;
-    li.className = "list"
-    ul.appendChild(li);
-    renderButtons(li);
-    input.value=""
-    addToDOLocalStorage(todoListValue)
-  }
-}
-  
-
-function updateCount() {
-  countSpan.textContent = ul.children.length;
+// taskcounter
+function taskCounter() {
+  countdisplay.textContent = ul.children.length;
 }
 
 
-function clearAllTasks() {
+// cleartasks
+function clearTasks() {
   ul.innerHTML = "";
 
-  todoListValue = [];
+  todoItems = [];
 
   localStorage.setItem("todos", JSON.stringify([]));
 
-  updateCount();
+  taskCounter();
 }
-clearAllBtn.addEventListener("click", clearAllTasks);
+clearAllBtn.addEventListener("click", clearTasks);
+
+
 
 showTodoList();
 
 
-function initEventListners() {
-  
+function showError(text) {
+  // errorDisplay.classList.remove('hide')
+  // errorDisplay.classList.add('show')
+  errorDisplay.style.visibility = 'visible'
 }
+
+
 
 
 
